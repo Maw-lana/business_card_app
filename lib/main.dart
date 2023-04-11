a6231735f6d6d57dfe4e892bb1f24908ac8ba5f7
Ahmed Fathelpap Mohamed <ahmed.fathelpap682000@gmail.com>
2023-04-12 00:01:17 +0200

    ListTile() استخدام Countainer() بدل

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,33 @@
 // ignore_for_file: avoid_unnecessary_containers
-
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const BusinessCardApp());
 }
-
 class BusinessCardApp extends StatelessWidget {
   const BusinessCardApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+
+      home:  Scaffold(
         backgroundColor: const Color.fromARGB(255, 153, 97, 0),
-        // ignore: prefer_const_literals_to_create_immutables
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const CircleAvatar(
+        body:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+           children: const [
+          CircleAvatar(
             radius: 122,
             backgroundColor: Colors.white,
             child: CircleAvatar(
-              backgroundImage: AssetImage('Images/Picsart_23-01-24_14-06-45-410.jpg'),
+              backgroundImage:
+                  AssetImage('Images/Picsart_23-01-24_14-06-45-410.jpg'),
               radius: 120,
             ),
           ),
-          const SizedBox(
+          SizedBox(
             height: 15,
           ),
-          const Text(
+          Text(
             'Ahmed Fath elpap',
             style: TextStyle(
               color: Colors.white,
@@ -36,67 +35,58 @@ class BusinessCardApp extends StatelessWidget {
               fontFamily: 'pacifico',
             ),
           ),
-          const SizedBox(
+          SizedBox(
             height: 5,
           ),
-          const Text(
+          Text(
             'FLUTTER DEVELOPER',
             style: TextStyle(
                 fontSize: 19,
                 fontWeight: FontWeight.w900,
-                color: Color.fromARGB(255, 187, 180, 119)
-                ),
+                color: Color.fromARGB(255, 187, 180, 119)),
           ),
-          const Divider(
+          Divider(
               color: Colors.white,
               thickness: 1.5,
               endIndent: 60,
               indent: 60,
-              height:40),
-              const SizedBox(height: 25,),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12), color: Colors.white),
-            // ignore: prefer_const_literals_to_create_immutables
-            child: Row(children: [
-              const Icon(Icons.call,
-                  size: 33, color: Color.fromARGB(255, 155, 78, 6)),
-              const Padding(
-                  padding: EdgeInsets.only(left: 16),
-                  child: Text(
-                    '+20 1029074709',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 46, 46, 49),
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ))
-            ]),
+              height: 40),
+          SizedBox(
+            height: 15,
           ),
-          const SizedBox(height: 20),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12), color: Colors.white),
-            // ignore: prefer_const_literals_to_create_immutables
-            child: Row(children: [
-              const Icon(Icons.mail,
-                  size: 33, color: Color.fromARGB(255, 155, 78, 6)),
-              const Padding(
-                padding: EdgeInsets.only(left: 16),
-                child: Text(
-                  'ahmed.fathelpap682000@gmail.com',
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 13),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: ListTile(
+                leading: Icon(Icons.call,
+                    size: 33, color: Color.fromARGB(255, 155, 78, 6)),
+                title: Text(
+                  '+20 1029074709',
                   style: TextStyle(
                     color: Color.fromARGB(255, 46, 46, 49),
-                    fontSize: 18,
+                    fontSize: 25,
                     fontWeight: FontWeight.bold,
                   ),
+                )),
+          ),
+          SizedBox(height: 10),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 13, vertical: 5),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: ListTile(
+              leading: Icon(Icons.mail,
+                  size: 33, color: Color.fromARGB(255, 155, 78, 6)),
+              title: Text(
+                'ahmed.fathelpap682000@gmail.com',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 46, 46, 49),
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ]),
+            ),
           ),
         ]),
       ),
